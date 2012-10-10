d4d0ee03d04194ba7371fe96b295d785ed1e437e
itsNikolay <itsnikolay@gmail.com>
2012-10-11 01:53:29 +0400

    Added db:seed

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '3.2.8'
 gem 'pg'
 gem 'kaminari'
 gem 'acts-as-taggable-on'
+gem "populator"
+gem "faker"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-require 'rubygems'
-require 'factory_girl'
-Dir[Rails.root.join("spec/factories.rb")].each {|f| require f}
 # encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
@@ -9,7 +6,28 @@ Dir[Rails.root.join("spec/factories.rb")].each {|f| require f}
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Factory.create(:user)
-15.times { Factory.create(:post, tag_list: "good, better, thebest") }
-15.times { Factory.create(:unpublishedpost, tag_list: "another, and, other") }
-15.times { Factory.create(:comment) }
+require 'faker'
+
+User.delete_all
+Post.delete_all
+Comment.delete_all
+3.times do |n|
+    email = Faker::Internet.email
+    password = "foobar"
+    password_confirmation = "foobar"
+    user = User.create!(email:    email,
+                 password: password,
+                 password_confirmation: password_confirmation)
+    3.times do |b|
+      post1 = user.posts.create! title: Faker::Lorem.sentence(5), text: Faker::Lorem.sentence(300), published: true, tag_list: "#{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}"
+      post2 = user.posts.create! title: Faker::Lorem.sentence(5), text: Faker::Lorem.sentence(300), published: false, tag_list: "#{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}"
+      3.times do |c|
+        pc1 = post1.comments.build content: Faker::Lorem.sentence(10)
+        pc1.user = user
+        pc1.save!
+        pc2 = post2.comments.build content: Faker::Lorem.sentence(10)
+        pc2.user = user
+        pc2.save!
+      end
+    end
+  end
