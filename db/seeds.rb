779f15fd72e0aed67ddcb391baa381bb0f9554e1
itsNikolay <itsnikolay@gmail.com>
2012-10-10 22:31:39 +0400

    required factory-girl

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'factory_girl_rails'
 # encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
