6f1c7ea54481fe9505d46514777a8f687b24522e
itsnikolay <itsnikolay@itsnikolay-Lenovo-G570.(none)>
2012-10-15 02:05:16 +0400

    Changed seed data, database encoding and some translations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,43 @@
 # encoding: utf-8
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'faker'
 
+#Before seeding we have to destroy all previous data.
 User.delete_all
 Post.delete_all
 Comment.delete_all
+
+title1 = %{Мост Кеннеди (Бонн)}
+text1 = %{Мост Кеннеди (нем. Kennedybrücke) — стальной сварной арочный мост через Рейн, расположенный в городе Бонне (Германия, федеральная земля Северный Рейн — Вестфалия) на расстоянии 654,9 км от истока реки. Мост соединяет городские районы Бёйель и Бонн (de: Bonn (Stadtbezirk)). По мосту проходит федеральная автодорога B56 (de: Bundesstraße 56).}
+
+title2 = %{Боа-Виста (аэропорт)}
+text2 = %{Международный аэропорт Боа-Виста – Атлас Бразил Кантанеди (порт. Aeroporto Internacional de Boa Vista - Atlas Brasil Cantanhede) (Код ИАТА: BVB) — бразильский аэропорт, расположенный в городе Боа-Виста, штат Рорайма.}
+
+comment1 = %{Хорошая статья. Жду продолжения.}
+comment2 = %{Могу выложить фотографии данного места.}
+
 3.times do |n|
-    email = Faker::Internet.email
+  email = "example#{n}@email.com"
     password = "foobar"
-    password_confirmation = "foobar"
-    user = User.create!(email:    email,
-                 password: password,
-                 password_confirmation: password_confirmation)
-    3.times do |b|
-      post1 = user.posts.create! title: Faker::Lorem.sentence(5), text: Faker::Lorem.sentence(300), published: true, tag_list: "#{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}"
-      post2 = user.posts.create! title: Faker::Lorem.sentence(5), text: Faker::Lorem.sentence(300), published: false, tag_list: "#{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}, #{Faker::Lorem.sentence(1)}"
-      3.times do |c|
-        pc1 = post1.comments.build content: Faker::Lorem.sentence(10)
-        pc1.user = user
-        pc1.save!
-        pc2 = post2.comments.build content: Faker::Lorem.sentence(10)
-        pc2.user = user
-        pc2.save!
-      end
+    user = User.create! email: email,
+                        password: password,
+                        password_confirmation: password
+  3.times do |b|
+    post1 = user.posts.create! title: title1, text: text1, published: true, tag_list: "тег-для-популярного, тег-для-нового, тег-для-полезного"
+    post2 = user.posts.create! title: title2, text: text2, published: false, tag_list: "тег-для-пользователей, тег-для-гостей, тег-для-раскрутки"
+    3.times do |c|
+      postcomment1 = post1.comments.build content: comment1
+      postcomment1.user = user
+      postcomment1.save!
+      postcomment2 = post1.comments.build content: comment2
+      postcomment2.user = user
+      postcomment2.save!
+      postcomment1 = post2.comments.build content: comment1
+      postcomment1.user = user
+      postcomment1.save!
+      postcomment2 = post2.comments.build content: comment2
+      postcomment2.user = user
+      postcomment2.save!
     end
   end
+end
+
+
